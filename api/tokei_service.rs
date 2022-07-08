408a04a8d2c38c5814aaf630f188982243fd6387
Austin Schey <aschey13@gmail.com>
2022-07-07 23:54:20 -0500

    add error handling

--- api/tokei_service.rs
+++ api/tokei_service.rs
@@ -1,6 +1,8 @@
 use badge::{Badge, BadgeOptions};
 use cached::Cached;
+use eyre::Context;
 use git2::{Direction, Remote, Repository};
+use log::info;
 use std::error::Error;
 use tempfile::TempDir;
 use tokei::{Config, Language, Languages};
@@ -21,28 +23,52 @@ const DAY_IN_SECONDS: u64 = 24 * 60 * 60;
 
 fn handler(req: Request) -> Result<impl IntoResponse, VercelError> {
     let (parts, _) = req.into_parts();
-
     let paths: Vec<_> = parts.uri.path().split('/').collect();
-
+    if paths.len() != 5 {
+        return Ok(Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .body(
+                "path must have the folllowing structure: /tokei/{domain}/{user}/{repo}"
+                    .to_string(),
+            )
+            .expect("Internal Server Error"));
+    }
     let (domain, user, repo) = (paths[2], paths[3], paths[4]);
-    let category = parts
-        .uri
-        .query()
-        .map(|q| q.split('=').collect::<Vec<_>>().get(1).unwrap().to_string())
-        .unwrap_or_else(|| String::from("lines"));
+    let category = "lines";
     let mut domain = percent_encoding::percent_decode_str(domain)
         .decode_utf8()
-        .unwrap();
-    // For backwards compatibility if a domain isn't specified we append `.com`.
+        .map_err(|e| VercelError::new(&e.to_string()[..]))?;
+
     if !domain.contains('.') {
         domain += ".com";
     }
-    let url = format!("https://{}/{}/{}", domain, user, repo);
 
-    let mut repo = Remote::create_detached(&url[..]).unwrap();
-    repo.connect(Direction::Fetch).unwrap();
-    let sha = repo.list().unwrap().first().unwrap().oid().to_string();
-    println!("{}", sha);
+    let url = format!("https://{}/{}/{}", domain, user, repo);
+    info!("Getting info for {url}");
+
+    let mut repo = match Remote::create_detached(&url[..]) {
+        Ok(repo) => repo,
+        Err(e) => return Ok(bad_request(e.to_string())),
+    };
+
+    if let Err(e) = repo.connect(Direction::Fetch) {
+        return Ok(bad_request(format!(
+            "Error connecting to repository: {}",
+            e
+        )));
+    }
+
+    let repo_list = match repo.list() {
+        Ok(list) => list,
+        Err(e) => return Ok(bad_request(format!("Error listing repo contents: {}", e))),
+    };
+
+    let sha = match repo_list.first() {
+        Some(sha) => sha.oid().to_string(),
+        None => return Ok(bad_request("Repo contains no refs".to_string())),
+    };
+    info!("Repo sha: {sha:?}");
+
     if let Some(badge) = CACHE
         .lock()
         .unwrap()
@@ -51,22 +77,32 @@ fn handler(req: Request) -> Result<impl IntoResponse, VercelError> {
         return Ok(Response::builder()
             .status(StatusCode::OK)
             .header("Content-Type", "image/svg+xml")
-            .body(make_badge(badge, &category[..]))
+            .header("Cache-Control", "s-maxage=60, stale-while-revalidate=300")
+            .body(make_badge(badge, category))
             .expect("Internal Server Error"));
     }
 
-    let entry = get_statistics(&url, &sha).unwrap();
-    let stats = entry.value;
+    let stats = get_statistics(&url, &sha)
+        .map_err(|e| VercelError::new(&e.to_string()[..]))?
+        .value;
 
-    let badge = make_badge(&stats, &category);
+    let badge = make_badge(&stats, category);
 
     let response = Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "image/svg+xml")
+        .header("Cache-Control", "s-maxage=60, stale-while-revalidate=300")
         .body(badge)
         .expect("Internal Server Error");
 
     Ok(response)
+}
+
+fn bad_request(body: String) -> Response<String> {
+    return Response::builder()
+        .status(StatusCode::BAD_REQUEST)
+        .body(body)
+        .expect("Internal Server Error");
 }
 
 fn repo_identifier(url: &str, sha: &str) -> String {
@@ -117,7 +153,7 @@ fn get_statistics(url: &str, _sha: &str) -> eyre::Result<cached::Return<Language
     let temp_dir = TempDir::new()?;
     let temp_path = temp_dir.path().to_str().unwrap();
 
-    Repository::clone(url, temp_path).unwrap();
+    Repository::clone(url, temp_path).wrap_err_with(|| "Error cloning repo")?;
 
     let mut stats = Language::new();
     let mut languages = Languages::new();
@@ -137,5 +173,6 @@ fn get_statistics(url: &str, _sha: &str) -> eyre::Result<cached::Return<Language
 
 #[allow(dead_code)]
 fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
     Ok(lambda!(handler))
 }
